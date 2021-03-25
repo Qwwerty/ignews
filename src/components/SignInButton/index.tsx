a23b477c5c3ba0664f94d08529b173ea90bb5993
DESKTOP-V5NHMSB\Rhalf <verdinhoraro2009@hotmail.com>
2021-03-25 07:46:36 -0300

    feat: autenticação

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -3,20 +3,25 @@ import styles from './styles.module.scss';
 
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client';
+
 
 export function SignInButton () {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
+  console.log(session);
+
+  return session ? (
     <button 
       type="button"
       className={styles.signInButton}
     >
       <FaGithub color="#04d361"/>
-      Rhalf Oliveira
+      {session.user.name}
       <FiX 
         color="#737380"
         className={styles.closeIcon}
+        onClick={() => signOut()}
       />
     </button>
   ) : 
@@ -24,6 +29,7 @@ export function SignInButton () {
     <button 
       type="button"
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417"/>
       Sing in with Github
